6b38ff08882fa5574ec684c0d721b9e08bf0f7e8
BrenoMelo-Profissional <caio2001ti@gmail.com>
2024-09-24 20:43:04 -0300

    Teste.py
    Primeira aula Senac

---
+++ SalarioBruto.py
@@ -0,0 +1,6 @@
+salario_base = float(input('Salario base: '))
+gratificaçao = float(input('Gratificação: '))
+
+salario_bruto = salario_base + gratificaçao
+
+print(f'O Valor do salario bruto é R$ {salario_bruto}')
---
+++ Teste.py
@@ -0,0 +1,52 @@
+
+
+# Teste
+
+print('Hello, world') # primeiro programa
+print('-' * 15)
+
+# Numero inteiro
+x = 10
+print(x)
+print(type(x))
+print('-' * 15)
+
+# Float ---- Numero flutuante ---- Numero decimal
+y = 3.14
+print(y)
+print(type(y))
+print('-' * 15)
+
+#  String ---- Qualquer texto e sempre estará dentro de aspas
+nome = 'Maria'
+print(nome)
+print(type(nome))
+print('-' * 15)
+
+# Lista [] ---- tipo mutavel , possivel de alteração
+lista = [1 , 2 , 3 , 4 , 5]
+print(lista)
+print(type(lista))
+print('-' * 15)
+
+# Tupla () ---- tipo imutavel , não é passivo de alteração
+tupla = (1 , 2 , 3 , 4 , 5)
+print(tupla)
+print(type(tupla))
+print('-' * 15)
+
+
+dicionario = {'nome' == 'Caio' , 'idade' == 22 , 'cidade' == 'Olinda'}
+print(dicionario)
+print(type(dicionario))
+print('-' * 15)
+
+# Numero boleano ---- Verdadeiro ou falso
+verdadeiro = True
+falso = False
+print(type(verdadeiro))
+print('-' * 15)
+
+# Numero nulo
+nulo = None
+print= (type(nulo))
